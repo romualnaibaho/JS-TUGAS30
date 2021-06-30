2132072cf4cd08a01846e2ce871291e2c28e2a6a
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 23:24:13 +0700

    js

---
+++ tugas30.js
@@ -0,0 +1,8 @@
+
+function regex(value){
+    var ambilData = value.match(/e|s/g);
+
+    console.log(ambilData);
+}
+
+regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
